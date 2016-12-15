9662bb6762f8ae24f63da93df3e207ad4924c8db
Jamie English <jamienglish@gmail.com>
2016-12-15 00:50:39 -0800

    Add S.and combinator

--- lib/speculation/core.rb
+++ lib/speculation/core.rb
@@ -1,4 +1,5 @@
 require 'concurrent/atom'
+require 'concurrent/delay'
 require 'hamster/hash'
 
 module Speculation
@@ -6,6 +7,9 @@ module Speculation
     REGISTRY = Concurrent::Atom.new(Hamster::Hash[])
 
     class Spec
+    end
+
+    class PredicateSpec
       def initialize(predicate)
         @predicate = predicate
       end
@@ -19,21 +23,66 @@ module Speculation
       end
     end
 
-    def self.def(name, &block)
-      spec = Spec.new(block)
+    class AndSpec < Spec
+      def initialize(specs)
+        @specs = specs
+      end
+
+      def conform(value)
+        # specs were delayed
+        @specs.value.each do |spec|
+          value = spec.conform(value)
+
+          if Speculation::Core.invalid?(value)
+            return :"Speculation::Core/invalid"
+          end
+        end
+
+        value
+      end
+    end
+
+    def self.def(name, spec)
+      unless spec.is_a?(Spec)
+        # More cases here!
+        spec = PredicateSpec.new(spec)
+      end
+
       REGISTRY.swap { |reg| reg.put(name, spec) }
+
       name
+    end
+
+    def self.reset_registry!
+      REGISTRY.swap { Hamster::Hash[] }
     end
 
     def self.conform(spec, value)
       spec = specize(spec)
+
       spec.conform(value)
     end
 
     def self.valid?(spec, value)
       spec = specize(spec)
-      !spec.conform(value).equal?(:"Speculation::Core/invalid")
+      value = spec.conform(value)
+
+      !invalid?(value)
     end
+
+    def self.invalid?(value)
+      value.equal?(:"Speculation::Core/invalid")
+    end
+
+    def self.and(*specs)
+      delayed_specs = Concurrent::Delay.new do
+        specs.map { |spec| specize(spec) }
+      end
+
+      AndSpec.new(delayed_specs)
+    end
+
+    ### private ###
 
     def self.specize(spec)
       case spec
@@ -41,7 +90,7 @@ module Speculation
       when Symbol then REGISTRY.value.fetch(spec)
       else
         if spec.respond_to?(:call)
-          Spec.new(spec)
+          PredicateSpec.new(spec)
         else
           raise ArgumentError,
             "spec: #{spec} must be a Spec, Symbol or callable, given #{spec.class}"
--- test/speculation_test.rb
+++ test/speculation_test.rb
@@ -4,18 +4,22 @@ require 'speculation/core'
 class SpeculationTest < Minitest::Test
   S = Speculation::Core
 
+  def setup
+    Speculation::Core.reset_registry!
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Speculation::VERSION
   end
 
   def test_conform_with_existing_spec
-    S.def(:integer) { |x| x.is_a?(Integer) }
+    S.def(:int?, -> (x) { x.is_a?(Integer) })
 
-    assert_equal 2, S.conform(:integer, 2)
-    assert_equal :"Speculation::Core/invalid", S.conform(:integer, "two")
+    assert_equal 2, S.conform(:int?, 2)
+    assert_equal :"Speculation::Core/invalid", S.conform(:int?, "two")
 
-    assert S.valid?(:integer, 2)
-    refute S.valid?(:integer, "two")
+    assert S.valid?(:int?, 2)
+    refute S.valid?(:int?, "two")
   end
 
   def test_conform_with_predicate
@@ -25,5 +29,20 @@ class SpeculationTest < Minitest::Test
 
     assert S.valid?(predicate, 2)
     refute S.valid?(predicate, "two")
+  end
+
+  def test_and_composition
+    S.def(:int?, -> (x) { x.is_a?(Integer) })
+    S.def(:even?, -> (x) { x.even? })
+
+    S.def(:big_even, S.and(:int?, :even?, -> (x) { x > 1000 }))
+
+    assert_equal :"Speculation::Core/invalid", S.conform(:big_even, :foo)
+    assert_equal :"Speculation::Core/invalid", S.conform(:big_even, 100)
+    assert_equal 1_000_000, S.conform(:big_even, 1_000_000)
+
+    refute S.valid?(:big_even, :foo)
+    refute S.valid?(:big_even, 10)
+    assert S.valid?(:big_even, 1_000_000)
   end
 end
