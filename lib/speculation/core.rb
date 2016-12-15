ca7f839e90d251d8078e286096d3fa923a189fd7
Jamie English <jamienglish@gmail.com>
2016-12-14 23:29:48 -0800

    Simple conform

---
+++ lib/speculation/core.rb
@@ -0,0 +1,33 @@
+require 'concurrent/atom'
+require 'hamster/hash'
+
+module Speculation
+  module Core
+    REGISTRY = Concurrent::Atom.new(Hamster::Hash[])
+
+    class Spec
+      def initialize(predicate)
+        @predicate = predicate
+      end
+
+      def conform(value)
+        if @predicate.call(value)
+          value
+        else
+          :"Speculation::Core/invalid"
+        end
+      end
+    end
+
+    def self.def(name, &block)
+      spec = Spec.new(block)
+      REGISTRY.swap { |reg| reg.put(name, spec) }
+      name
+    end
+
+    def self.conform(spec_name, value)
+      spec = REGISTRY.value.fetch(spec_name)
+      spec.conform(value)
+    end
+  end
+end
--- test/speculation_test.rb
+++ test/speculation_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
+require 'speculation/core'
 
 class SpeculationTest < Minitest::Test
+  S = Speculation::Core
+
   def test_that_it_has_a_version_number
     refute_nil ::Speculation::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_conform_with_simple_predicate
+    S.def(:integer) { |x| x.is_a?(Integer) }
+
+    assert_equal 2, S.conform(:integer, 2)
+    assert_equal :"Speculation::Core/invalid", S.conform(:integer, "two")
   end
 end
