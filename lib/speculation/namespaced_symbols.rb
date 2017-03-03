b9a3ea59d7aa03d8ab198753ea0eeff16bd01825
Jamie English <jamienglish@gmail.com>
2017-03-02 19:34:37 -0800

    refactor ns syms
    allow for extraction into opt-in class method vs refinement vs monkey
    patch

--- lib/speculation/namespaced_symbols.rb
+++ lib/speculation/namespaced_symbols.rb
@@ -6,23 +6,32 @@ module Speculation
       Module.new do
         refine Symbol do
           define_method(:ns) do |mod = nil|
-            if mod
-              :"#{mod}/#{self}"
-            else
-              :"#{namespace}/#{self}"
-            end
+            mod ||= namespace
+            NamespacedSymbols.symbol(mod, self)
           end
 
           def name
-            to_s.split("/").last
+            NamespacedSymbols.name(self)
           end
 
           def namespace
-            parts = to_s.split("/")
-            parts.first if parts.count == 2
+            NamespacedSymbols.namespace(self)
           end
         end
       end
+    end
+
+    def self.symbol(ns, name)
+      :"#{ns}/#{name}"
+    end
+
+    def self.name(sym)
+      sym.to_s.split("/").last
+    end
+
+    def self.namespace(sym)
+      parts = sym.to_s.split("/")
+      parts.first if parts.count == 2
     end
   end
 end
