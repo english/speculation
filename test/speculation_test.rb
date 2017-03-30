f4d24850cfa04c3ae43d65daba1a06c02a462c81
Jamie English <jamienglish@gmail.com>
2017-03-30 14:49:17 -0700

    Tidy up test

--- test/speculation_test.rb
+++ test/speculation_test.rb
@@ -7,16 +7,6 @@ module Speculation
     S = Speculation
     include S::NamespacedSymbols
 
-    def test_conform_with_existing_spec
-      S.def(ns(:int?), ->(x) { x.is_a?(Integer) })
-
-      assert_equal 2, S.conform(ns(:int?), 2)
-      assert_equal :"Speculation/invalid", S.conform(ns(:int?), "two")
-
-      assert S.valid?(ns(:int?), 2)
-      refute S.valid?(ns(:int?), "two")
-    end
-
     def test_def_requires_namespaced_symbol
       assert_raises(ArgumentError) do
         S.def("foo/integer", Integer)
@@ -25,6 +15,16 @@ module Speculation
       assert_raises(ArgumentError) do
         S.def(:integer, Integer)
       end
+    end
+
+    def test_conform_with_existing_spec
+      S.def(ns(:int?), ->(x) { x.is_a?(Integer) })
+
+      assert_equal 2, S.conform(ns(:int?), 2)
+      assert_equal :"Speculation/invalid", S.conform(ns(:int?), "two")
+
+      assert S.valid?(ns(:int?), 2)
+      refute S.valid?(ns(:int?), "two")
     end
 
     def test_conform_with_predicate
@@ -40,33 +40,18 @@ module Speculation
       S.def(ns(:wont_conform_keys), S.hash_of(S.and(Symbol, S.conformer(&:to_s)),
                                               S.and(Float, S.conformer(&:to_i))))
 
-      assert_equal({ :foo => 1, :bar => 2 },
-                   S.conform(ns(:wont_conform_keys), :foo => 1.0, :bar => 2.0))
+      assert_equal Hash[:foo => 1, :bar => 2],
+                   S.conform(ns(:wont_conform_keys), :foo => 1.0, :bar => 2.0)
 
       S.def(ns(:will_conform_keys), S.hash_of(S.and(Symbol, S.conformer(&:to_s)),
                                               S.and(Float, S.conformer(&:to_i)),
                                               :conform_keys => true))
 
-      assert_equal({ "foo" => 1, "bar" => 2 },
-                   S.conform(ns(:will_conform_keys), :foo => 1.0, :bar => 2.0))
+      assert_equal Hash["foo" => 1, "bar" => 2],
+                   S.conform(ns(:will_conform_keys), :foo => 1.0, :bar => 2.0)
     end
 
     def test_explain_data
-      S.def(ns(:even), ->(x) { x.even? })
-
-      ed = S.explain_data(ns(:even), 1)
-      problems = ed.fetch(ns(S, :problems))
-
-      assert_equal 1, problems.count
-
-      problem = problems.first
-      assert_equal [], problem[:path]
-      assert_equal 1, problem[:val]
-      assert_equal [ns(:even)], problem[:via]
-      assert_equal [], problem[:in]
-      assert_kind_of Proc, problem[:pred].first
-      assert_equal [1], problem[:pred].last
-
       S.def(ns(:integer), Integer)
       S.def(ns(:even), ->(x) { x.even? })
       S.def(ns(:even_integer), S.and(ns(:integer), ns(:even)))
@@ -76,12 +61,11 @@ module Speculation
 
       assert_equal 1, problems.count
 
-      problem = problems.first
-      assert_equal [], problem[:path]
-      assert_equal "s", problem[:val]
-      assert_equal [ns(:even_integer), ns(:integer)], problem[:via]
-      assert_equal [], problem[:in]
-      assert_equal [Integer, ["s"]], problem[:pred]
+      assert_equal Hash[:path => [],
+                        :val  => "s",
+                        :via  => [ns(:even_integer), ns(:integer)],
+                        :in   => [],
+                        :pred => [Integer, ["s"]]], problems.first
     end
   end
 end
